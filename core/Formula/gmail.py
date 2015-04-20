339fc173e857cc50212fe785f21feaf9b9945d00
darker0n <dark.crak@ya.ru>
2015-04-20 16:53:49 +0300

    Reformated code. Added formula files. Bug fixes.

---
+++ core/Formula/gmail.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import webbrowser
+import core.wcolors
+
+print(core.wcolors.color.GREEN + "Opening Gmail..." + core.color.ENDC)
+webbrowser.open('https://mail.google.com/mail')
