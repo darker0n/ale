02c0dc5e64b647f6ead93180440b1b1a4ad27ac6
darker0n <dark.crak@ya.ru>
2015-04-21 20:15:19 +0300

    Search in Ask.

--- core/Formula/images.py
+++ core/Formula/images.py
@@ -8,7 +8,7 @@ class Formula():
         self.request = request
 
     def search(self):
-        print(wcolors.color.GREEN + "Searching in Google Images=> " + wcolors.color.ENDC + wcolors.color.RED + parser.parse_request(self.request[0:]) + wcolors.color.ENDC)
+        print(wcolors.color.GREEN + "Searching in Google Images => " + wcolors.color.ENDC + wcolors.color.RED + parser.parse_request(self.request[0:]) + wcolors.color.ENDC)
 
         webbrowser.open('https://www.google.com/search?q=' + parser.parse_request(self.request[0:]) + "&tbm=isch")
 
