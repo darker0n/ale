f6abf20ef72e6e4d030de4ebe2737e904f9786c1
darker0n <dark.crak@ya.ru>
2015-04-21 19:55:30 +0300

    Added Twitter and Google Drive.

--- core/Formula/drive.py
+++ core/Formula/drive.py
@@ -9,8 +9,8 @@ class Formula():
         self.request = request
 
     def open(self):
-        print(wcolors.color.GREEN + "Opening Gmail..." + wcolors.color.ENDC)
-        webbrowser.open('https://mail.google.com')
+        print(wcolors.color.GREEN + "Opening Google Drive..." + wcolors.color.ENDC)
+        webbrowser.open('https://drive.google.com')
 
     def main(self):
         self.open()
