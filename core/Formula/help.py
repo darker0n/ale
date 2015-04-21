70e08db7c5096c80a76ad9c699a7ccf5a9aea216
darker0n <dark.crak@ya.ru>
2015-04-21 21:08:17 +0300

    Added help command.

---
+++ core/Formula/help.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import webbrowser
+import wcolors
+
+
+class Formula():
+    def __init__(self, request):
+        self.request = request
+
+    def open(self):
+        print(wcolors.color.GREEN + "Opening Ale Wiki..." + wcolors.color.ENDC)
+        webbrowser.open('https://github.com/darker0n/ale/wiki')
+
+    def main(self):
+        self.open()
