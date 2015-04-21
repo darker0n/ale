963fbfea82d64ee2138800bebdc424e668ecb532
darker0n <11329474+darker0n@users.noreply.github.com>
2015-04-21 18:20:44 +0300

    Bug fixes in Formula files. Reformat code.

---
+++ core/Formula/rotten.py
@@ -0,0 +1,18 @@
+import webbrowser
+import wcolors
+
+from core import parser
+
+
+class Formula():
+    def __init__(self, request):
+        self.request = request
+
+    def search(self):
+        print(
+            wcolors.color.GREEN + "Searching in Rotten Tomatoes => " + wcolors.color.ENDC + wcolors.color.RED + parser.parse_request(
+                self.request[0:]) + wcolors.color.ENDC)
+        webbrowser.open('http://www.rottentomatoes.com/search/?search=' + parser.parse_request(self.request[0:]))
+
+    def main(self):
+        self.search()
