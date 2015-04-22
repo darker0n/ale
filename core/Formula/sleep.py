78692bd24658e6e574e797050ef3471afa34a9d3
darker0n <11329474+darker0n@users.noreply.github.com>
2015-04-22 21:00:16 +0300

    Go to sleep command.

---
+++ core/Formula/sleep.py
@@ -0,0 +1,14 @@
+import subprocess
+import wcolors
+
+
+class Formula():
+    def __init__(self, request):
+        self.request = request
+
+    def sleep(self):
+        print(wcolors.color.GREEN + "Going to sleep..." + wcolors.color.ENDC)
+        subprocess.call(['osascript', '-e', 'tell app "System Events" to sleep'])
+
+    def main(self):
+        self.sleep()
