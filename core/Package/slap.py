78e7f91cc85bea94c54bfca4ce3ef71560263c5c
darker0n <11329474+darker0n@users.noreply.github.com>
2015-04-26 12:53:15 +0300

    Slap formula.

---
+++ core/Package/slap.py
@@ -0,0 +1,16 @@
+from core import wcolors
+import os
+
+
+class Package():
+    def __init__(self, request):
+        self.request = request
+        self.name = "Slap"
+        self.version = "0.1.24"
+
+    def install(self):
+        print(wcolors.color.GREEN + "Installing => " + wcolors.color.ENDC + wcolors.color.RED + self.name + wcolors.color.ENDC)
+        os.system("curl -sL https://raw.githubusercontent.com/slap-editor/slap/master/install.sh | sh")
+
+    def main(self):
+        self.install()
