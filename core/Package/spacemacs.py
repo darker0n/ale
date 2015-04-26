27158ee846b5b1c48d3620770f0edbf5cb8909a3
darker0n <dark.crak@ya.ru>
2015-04-26 17:30:48 +0300

    Spacemacs formula.

---
+++ core/Package/spacemacs.py
@@ -0,0 +1,16 @@
+from core import wcolors
+import os
+
+
+class Package():
+    def __init__(self, request):
+        self.request = request
+        self.name = "Spacemacs"
+        self.version = "24.5.1"
+
+    def install(self):
+        print(wcolors.color.GREEN + "Installing => " + wcolors.color.ENDC + wcolors.color.RED + self.name + wcolors.color.ENDC)
+        os.system("git clone --recursive http://github.com/syl20bnr/spacemacs ~/.emacs.d")
+
+    def main(self):
+        self.install()
