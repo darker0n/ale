3f8dfedcd0155bc82b2649994d0e68b125486eae
darker0n <dark.crak@ya.ru>
2015-04-29 20:50:30 +0300

    Install function instead of main in package manager

--- core/Package/brew.py
+++ core/Package/brew.py
@@ -11,6 +11,3 @@ class Package():
     def install(self):
         print(wcolors.color.GREEN + "Installing => " + wcolors.color.ENDC + wcolors.color.RED + self.name + wcolors.color.ENDC)
         os.system("ruby -e \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)\"")
-
-    def main(self):
-        self.install()
--- core/Package/slap.py
+++ core/Package/slap.py
@@ -11,6 +11,3 @@ class Package():
     def install(self):
         print(wcolors.color.GREEN + "Installing => " + wcolors.color.ENDC + wcolors.color.RED + self.name + wcolors.color.ENDC)
         os.system("curl -sL https://raw.githubusercontent.com/slap-editor/slap/master/install.sh | sh")
-
-    def main(self):
-        self.install()
